9c24c1ea57e5c435995b33b827be8ef9c5fa77d1
Artem Korniienko <tmadmanar2307@gmail.com>
2026-05-05 17:44:53 +0100

    Final tweaks

--- OpenGLTemplate/FreeCam.cpp
+++ OpenGLTemplate/FreeCam.cpp
@@ -1,56 +1,12 @@
-#include "gamewindow.h"
 #include "FreeCam.h"
+#include "gamewindow.h"
 #include "Common.h"
-#include "./include/glm/gtc/type_ptr.hpp"
-#include "./include/glm/gtc/matrix_transform.hpp"
 
 FreeCam::FreeCam()
 {
 	m_camData.position = glm::vec3();
-	m_camData.upVector = glm::vec3();
-	m_camData.viewpoint = glm::vec3();
-}
-
-// Respond to mouse movement
-void FreeCam::SetViewByMouse()
-{
-	int middle_x = GameWindow::SCREEN_WIDTH >> 1;
-	int middle_y = GameWindow::SCREEN_HEIGHT >> 1;
-
-	float angle_y = 0.0f;
-	float angle_z = 0.0f;
-	static float rotation_x = 0.0f;
-
-	POINT mouse;
-	GetCursorPos(&mouse);
-
-	if (mouse.x == middle_x && mouse.y == middle_y) {
-		return;
-	}
-
-	SetCursorPos(middle_x, middle_y);
-
-	angle_y = (float)(middle_x - mouse.x) / 10000.0f;
-	angle_z = (float)(middle_y - mouse.y) / 10000.0f;
-
-	rotation_x -= angle_z;
-
-	float maxAngle = 1.56f; // Just a little bit below PI / 2
-
-	if (rotation_x > maxAngle) {
-		rotation_x = maxAngle;
-	}
-	else if (rotation_x < -maxAngle) {
-		rotation_x = -maxAngle;
-	}
-	else {
-		glm::vec3 cross = glm::cross(m_camData.viewpoint - m_camData.position, m_camData.upVector);
-		glm::vec3 axis = glm::normalize(cross);
-
-		RotateViewPoint(angle_z, axis);
-	}
-
-	RotateViewPoint(angle_y, glm::vec3(0, 1, 0));
+	m_camData.upVector = glm::vec3(0.0f, 1.0f, 0.0f);
+	m_camData.viewpoint = glm::vec3(0.0f, 0.0f, -1.0f);
 }
 
 camData FreeCam::GetData(double dt, const camData& currentData)
@@ -60,13 +16,44 @@ camData FreeCam::GetData(double dt, const camData& currentData)
 	return m_camData;
 }
 
-// Rotate the camera view point -- this effectively rotates the camera since it is looking at the view point
-void FreeCam::RotateViewPoint(float fAngle, const glm::vec3 vPoint)
+void FreeCam::SetViewByMouse()
 {
-	glm::vec3 vView = m_camData.viewpoint - m_camData.position;
+	int middle_x = GameWindow::SCREEN_WIDTH >> 1;
+	int middle_y = GameWindow::SCREEN_HEIGHT >> 1;
 
-	glm::mat4 R = glm::rotate(glm::mat4(1), fAngle * 180.0f / (float)M_PI, vPoint);
-	glm::vec4 newView = R * glm::vec4(vView, 1);
+	POINT mouse;
+	GetCursorPos(&mouse);
 
-	m_camData.viewpoint = m_camData.position + glm::vec3(newView);
+	if (mouse.x == middle_x && mouse.y == middle_y)
+	{
+		return;
+	}
+
+	SetCursorPos(middle_x, middle_y);
+
+	float delta_x = (float)(middle_x - mouse.x);
+	float delta_y = (float)(middle_y - mouse.y);
+
+	float sensitivity = 0.0015f;
+
+	glm::vec3 viewDir = glm::normalize(m_camData.viewpoint - m_camData.position);
+
+	float pitch = std::asin(viewDir.y);
+	float yaw = std::atan2(viewDir.z, viewDir.x);
+
+	yaw -= delta_x * sensitivity;
+	pitch += delta_y * sensitivity;
+
+	float maxPitch = glm::radians(89.0f);
+	if (pitch > maxPitch) pitch = maxPitch;
+	if (pitch < -maxPitch) pitch = -maxPitch;
+
+	glm::vec3 newView;
+	newView.x = std::cos(yaw) * std::cos(pitch);
+	newView.y = std::sin(pitch);
+	newView.z = std::sin(yaw) * std::cos(pitch);
+
+	m_camData.viewpoint = m_camData.position + glm::normalize(newView);
+
+	m_camData.upVector = glm::vec3(0.0f, 1.0f, 0.0f);
 }
--- OpenGLTemplate/FreeCam.h
+++ OpenGLTemplate/FreeCam.h
@@ -8,9 +8,6 @@ public:
 
 	camData GetData(double dt, const camData& currentData) override;
 private:
-	// Rotate the camera viewpoint -- this effectively rotates the camera
-	void RotateViewPoint(float angle, const glm::vec3 viewPoint);
-
 	// Respond to mouse movement to rotate the camera
 	void SetViewByMouse();
 	camData m_camData;
